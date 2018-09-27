14658ccb82214c33df3e3cbd5d2b22dea552b8ce
Alasdair Hendry <alasdair@megabitestudios.uk>
2018-09-27 15:02:06 +0100

    Basic Multiplayer Foundation
    -Peer-to-peer connections made
    -Distinguishing Host/Client
    -Basic server to client communications
    -Simple player movement created

---
+++ The Mayhem Pits/Assets/NetworkPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkPlayer : NetworkBehaviour {
+
+    [SerializeField] private GameObject localPlayer;
+
+    [SerializeField] [SyncVar] private string playerName;
+    [SerializeField] [SyncVar] private string playerHealth;
+
+    private void Start()
+    {
+        if (!isLocalPlayer) return;
+        CmdSpawnLocalPlayer();
+    }
+
+    [Command]
+    private void CmdSpawnLocalPlayer()
+    {
+        GameObject _localPlayer = Instantiate(localPlayer, transform.position, Quaternion.identity);
+        NetworkServer.SpawnWithClientAuthority(_localPlayer, connectionToClient);
+    }
+
+    //private void Start()
+    //{
+    //    GetComponentInChildren<TextMesh>().text = playerName;
+
+    //    if (!isLocalPlayer) return;
+    //    RandomiseName();       
+    //}
+
+    //private void Update()
+    //{
+    //    if (!isLocalPlayer) return;
+    //    if (Input.GetKeyDown(KeyCode.R)) RandomiseName();
+    //}
+
+    //void RandomiseName()
+    //{
+    //    gameObject.name = "Player-" + Random.Range(101, 1000);
+    //    CmdUpdateName(gameObject.name);
+    //}
+
+    //[Command]
+    //void CmdUpdateName(string name)
+    //{
+    //    playerName = name;
+    //    RpcUpdateName(name);
+    //}
+
+    //[ClientRpc]
+    //void RpcUpdateName(string name)
+    //{
+    //    playerName = name;
+    //    GetComponentInChildren<TextMesh>().text = playerName;
+    //}
+
+    ////void Hook_OnNameChanged(string name)
+    ////{
+    ////    playerName = name;
+    ////    
+    ////}
+}
---
+++ The Mayhem Pits/Assets/RobotMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RobotMovement : NetworkBehaviour {
+
+    [SerializeField] private float movementSpeed;
+
+    private Rigidbody rb;
+    private Vector3 movementVector = new Vector3();
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    private void Update () {
+        if (!hasAuthority) return;
+        MovementInput();
+	}
+
+    private void MovementInput()
+    {
+        Vector3 forwardMovement = Input.GetAxis("Vertical") * transform.forward;
+        Vector3 sidewaysMovement = Input.GetAxis("Horizontal") * transform.right;
+
+        movementVector = (forwardMovement + sidewaysMovement).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasAuthority) return;
+        Movement();
+        Rotation();
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(transform.position + (movementVector * movementSpeed * Time.fixedDeltaTime));
+    }
+
+    private void Rotation()
+    {
+
+    }
+}
